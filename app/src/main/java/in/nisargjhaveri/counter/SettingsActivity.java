4c10f3c4104038e4c3a1baddce35ce36e405c3a1
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2016-06-06 10:13:46 +0530

    Remove unused imports

--- app/src/main/java/in/nisargjhaveri/counter/SettingsActivity.java
+++ app/src/main/java/in/nisargjhaveri/counter/SettingsActivity.java
@@ -1,6 +1,5 @@
 package in.nisargjhaveri.counter;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -9,11 +8,7 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
